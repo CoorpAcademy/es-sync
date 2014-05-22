171d8a4d508ac097b70504d06b9ebf593ae3f077
julien <julien@3dw.org>
2014-05-22 14:20:51 +0200

    Allow passing extra command line arguments to the executable

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function sync_full(completion_handler)
+function sync_full(completion_handler, options)
 {
-	return es_sync('full', completion_handler);
+	return es_sync('full', completion_handler, options);
 }
 
-function sync_incremental(completion_handler)
+function sync_incremental(completion_handler, options)
 {
-	return es_sync('incremental', completion_handler);
+	return es_sync('incremental', completion_handler, options);
 }
 
-function es_sync(type, completion_handler)
+function es_sync(type, completion_handler, options)
 {
 	if (((process.platform == 'linux') || (process.platform == 'darwin')) && (process.arch == 'x64'))
 	{
@@ -16,7 +16,16 @@ function es_sync(type, completion_handler)
 		var exeName = 'coorp_es_sync_' + process.platform + '_' + process.arch;
 		var exePath = path.join(__dirname, './' + exeName);
 		var confPath = path.join(__dirname, './config.xml');
-		sync_process = require('child_process').execFile(exePath, ['--conf', confPath, '--type', type], 
+		var args = ['--conf', confPath, '--type', type];
+		if (options !== null)
+		{
+			for (var i in options) {
+				if (true) {
+					args.push(i, options[i]);
+				}
+			}
+		}
+		sync_process = require('child_process').execFile(exePath, args,
 				{ maxBuffer: 1024*1024 }, completion_handler);
 		return true;
 	}else{
